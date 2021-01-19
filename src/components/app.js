61a2f0a17b9c498a3569ce1e283f50247fa23a3e
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-01-19 14:11:49 +0100

    Create and set initial value for currentCampers, allTimeCampers. Create recent and allTime buttons to alternate between recent and allTime campers

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            recentCampers: [],
+            allTimeCampers: [],
+            currentView: 'recentCampers'
+        }
+    }
+
+    componentDidMount() {
+        // make concurrent requests and set state to response
+        axios.all([this.fetchRecentCampers(), this.fetchAllTimeCampers()])
+            .then(axios.spread(function(recentCampers, allTimeCampers) {
+               this.setState({ recentCampers, allTimeCampers }) 
+        }))
+    }
+
+    fetchRecentCampers() {
+        return axios.get('https://swapi.dev/api/starships/')
+    }
+
+    fetchAllTimeCampers() {
+        return axios.get('https://swapi.dev/api/species/')
+    }
+
+    handleView(currentView) {
+        this.setState({ currentView })
+    }
+
   render() {
     return (
-      <div>React simple starter</div>
+        <div>
+            <h2>{`Viewing Top ${this.state.currentView}`}</h2>
+            <button 
+                className="btn btn-primary"
+                onClick={() => this.handleView('recentCampers')}>
+                Recent
+            </button>
+            <button 
+                className="btn btn-primary"
+                onClick={() => this.handleView('allTimeCampers')}>
+                All Time
+            </button>
+        </div>
     );
   }
 }
